be428dc71ad6a689164fc57dbbb815270e9f2007
Swechha Humagain <swechha2003@gmail.com>
2024-04-02 23:01:38 +0545

    first project in java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+/**
+ * The following class contains the main method for the project that initializes the objects and displays the output.
+ * The program works with pre-input values in its current state but will have GUI functionalities and output when complete
+ */
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    ArrayList<Integer> fullMarksList = new ArrayList<Integer>();
-    ArrayList<Integer> obtainedMarksList = new ArrayList<Integer>();
 
+    /**
+     * The main method is used to initialize  the objects and display the output which is done below
+     * @param args
+     */
     public static void main(String[] args) {
+        //creating a new object
+        SubjectMarksDetails subject1 = new SubjectMarksDetails("Science", 100, 89);
+        SubjectMarksDetails subject2 = new SubjectMarksDetails("Maths", 100, 95);
+        SubjectMarksDetails subject3 = new SubjectMarksDetails("Computer", 100, 80);
 
-        
+        //adding the percentage of individual subjects
+        double totalPercentage = (subject1.calculatePercentage(subject1) + subject2.calculatePercentage(subject2) + subject3.calculatePercentage(subject3)) / 3;
 
+        //prints the total percentage
+        System.out.println("Your percentage is: " + totalPercentage);
+        //prints whether the student has passed or failed
+        if (totalPercentage >= 40) {
+            System.out.println(" Congratulations! You have passed");
+        } else {
+            System.out.println(" Sorry! You have failed. Please try again ");
         }
     }
-,
+}
--- src/SubjectMarksDetails.java
+++ src/SubjectMarksDetails.java
@@ -1,28 +1,35 @@
-import java.util.ArrayList;
+/**
+ * The following class initializes the variables and methods to be used.
+ * In its current state the program can calculate the percentage and determine if passed or failed for pre-established objects.
+ * However, with newer iterations, it will use user-input to perform calculations.
+ */
 
 public class SubjectMarksDetails {
+    private String subjectName;// variable used to store name of the subject
+    private int subjectFullmarks;// variable used to store full marks obtained in a subject
+    private int subjectObtainedMarks; // variable used to store obtained marks obtained in a subject
 
-    public int getFullMarks(ArrayList<Integer> list){
-        int fullMarksAddition = 0;
-        for (int a : list){
-            fullMarksAddition = fullMarksAddition + a;
-        }
-        return fullMarksAddition;
+
+
+
+
+    //constructor for subject marks details class to initialize  variables
+    public SubjectMarksDetails( String subjectName,int subjectFullmarks, int subjectObtainedMarks){
+        this.subjectName = subjectName;
+        this.subjectFullmarks = subjectFullmarks;
+        this.subjectObtainedMarks = subjectObtainedMarks;
     }
-    public int getObtainedMarks(ArrayList<Integer> newlist){
-        int obtainedMarksAddition = 0;
-        for (int b : newlist){
-            obtainedMarksAddition = obtainedMarksAddition + b;
-        }
-        return obtainedMarksAddition;
-    }
-     public int calculatePercentage(int fullmarks, int obtainedmarks){
-        int percentage = (obtainedmarks/fullmarks)*100;
+
+    /**
+     * The following method is used to calculate the percentage of individual subjects that can be calculated to determine the
+     * total percentage of the student in all subjects
+     * @param s
+     * @return percentage
+     */
+     public  double calculatePercentage(SubjectMarksDetails s){
+         //calculate the percentage of individual subjects
+        double percentage = ((double)s.subjectObtainedMarks/s.subjectFullmarks)*100;
         return percentage;
      }
-     public boolean checkPassorFail(int percent){
-        boolean passOrFail = false;
-         passOrFail = percent >= 40;
-        return passOrFail;
-     }
+
 }
