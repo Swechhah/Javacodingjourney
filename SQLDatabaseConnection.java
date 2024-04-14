6f081008df556accb63a7c81c1d8e10e760aecdd
Swechha Humagain <148635549+Swechhah@users.noreply.github.com>
2024-04-14 23:46:49 +0545

    Update and rename Main.java to SQLDatabaseConnection.java

--- SQLDatabaseConnection.java
+++ SQLDatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main {
+public class SQLDatabaseConnection {
     public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3306/portfolio_management_system";
