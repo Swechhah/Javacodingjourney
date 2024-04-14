28905e593761550dd8068f7533ba4aa6e6892882
Swechha Humagain <148635549+Swechhah@users.noreply.github.com>
2024-04-14 23:57:08 +0545

    Update SQLDatabaseConnection.java

--- SQLDatabaseConnection.java
+++ SQLDatabaseConnection.java
@@ -3,29 +3,36 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+/*
+* The code is designed to display the connection between mysql database and java code.
+The code checks the connection for the database built previously and responds accordingly
+*/
 
 public class SQLDatabaseConnection {
+    /*
+    * The main method executes the program to display whether the database connection has been established or not
+    */
     public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3306/portfolio_management_system";
         String user = "root";
         String password = "";
 
-
-        try {
+        
+        try {//searches for the external jar connector
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {//catch lack of driver in the project classpath
             e.printStackTrace();
             return;
         }
-
+        // try catch to connect to database using username and password
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             if (connection != null) {
                 System.out.println("Connected to the database!");
             } else {
                 System.out.println("Failed to make connection!");
             }
-        } catch (SQLException e) {
+        } catch (SQLException e) {//catch lack of datbase connectuon
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         }
     }
