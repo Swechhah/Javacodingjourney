f3c837bf93647731519e5f137a1d0a8a9d60a980
swechhah <np01cp4s230128@islingtoncollege.edu.np>
2024-04-16 23:25:05 +0545

    First commit

---
+++ src/main/java/com/example/portfoliomanagementsystem/PortfolioManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfoliomanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfoliomanagementsystem/PortfolioManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfoliomanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
