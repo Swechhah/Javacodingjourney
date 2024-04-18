a75419451f0f4335479f08d4b091be60bb525a07
swechhah <np01cp4s230128@islingtoncollege.edu.np>
2024-04-19 00:14:06 +0545

    commit

---
+++ src/main/java/com/example/portfoliomanagementsystem/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.portfoliomanagementsystem;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping
+    public String helloWorld() {
+        return "Hello World from Spring Boot!";
+    }
+}
