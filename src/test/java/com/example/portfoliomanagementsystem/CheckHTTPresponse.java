037bc80527f77beef2c4e66f8f8c5ca3c978655f
swechhah <np01cp4s230128@islingtoncollege.edu.np>
2024-04-20 23:14:59 +0545

    commit

--- src/test/java/com/example/portfoliomanagementsystem/CheckHTTPresponse.java
+++ src/test/java/com/example/portfoliomanagementsystem/CheckHTTPresponse.java
@@ -7,19 +7,25 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// This annotation indicates that this class is a test class and will be managed by the Spring Boot test environment.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CheckHTTPresponse {
+    // This annotation injects the actual port that was randomly assigned for testing.
     @LocalServerPort
     private int port;
 
-        @Autowired
+    // This annotation autowires an instance of TestRestTemplate, which is a convenient way to test RESTful services in Spring Boot applications.
+    @Autowired
     private TestRestTemplate testRestTemplate;
 
+    // This method tests if the HTTP response from the server matches the expected string.
     @Test
     public void shouldPassIfStringMatches(){
+        // This assertion checks if the response from the server matches the expected string.
         assertEquals("Hello World from Spring Boot",
+                // This method sends a GET request to the specified URL (constructed using the injected port) and retrieves the response as a String.
                 testRestTemplate.getForObject("http://localhost:" + port + "/",
-                    String.class));
-
+                        String.class));
     }
 }
+
