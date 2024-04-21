b93e89c634fce06799163f609ed5542100928e59
swechhah <np01cp4s230128@islingtoncollege.edu.np>
2024-04-21 22:50:54 +0545

    commit

--- src/main/java/com/example/portfoliomanagementsystem/HelloWorldController.java
+++ src/main/java/com/example/portfoliomanagementsystem/HelloWorldController.java
@@ -3,10 +3,14 @@ package com.example.portfoliomanagementsystem;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// This annotation marks this class as a controller that handles web requests and returns the response.
 @RestController
 public class HelloWorldController {
+    // This annotation maps HTTP requests to handler methods of this controller.
     @RequestMapping
     public String helloWorld() {
+        // This method returns the string "Hello World from Spring Boot" as the response to any HTTP request.
         return "Hello World from Spring Boot";
     }
 }
+
